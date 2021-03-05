effc82c8300b4cca7601e7b3415c96a72ecf4e7a
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2021-03-05 23:56:18 +0200

    2021/0205 23:56

---
+++ JS/slick-carusel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+       $('.sort-slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: '.prev-arrow',
+    nextArrow: '.next-arrow',
+  }); 
+})
+
---
+++ JS/validation.js
@@ -0,0 +1,53 @@
+function onSubClick(event) {
+    event.preventDefault();
+
+    let firstName = document.getElementById("name-id").value;
+    let lastName = document.getElementById("last-id").value;
+    let email = document.getElementById("mail-id").value;
+    let massage = document.getElementById("tel-id").value;
+
+    document.getElementById('name-span').innerHTML = '';
+    document.getElementById('last-span').innerHTML = '';
+    document.getElementById('mail-span').innerHTML = '';
+    document.getElementById('tel-span').innerHTML = '';
+
+    if (firstName < 2) {
+        document.getElementById('name-span').innerHTML = "Your First Name Is Too Short!"
+       document.getElementById('name-span').style.color =  "red";
+    }
+    if (lastName < 2) {
+         document.getElementById('last-span').innerHTML = "Your Last Name Is Too Short!"
+        document.getElementById('last-span').style.color =  "red";
+    }
+    if (email.indexOf("@") == -1 || email.indexOf(".") == -1  ) {
+        document.getElementById('mail-span').innerHTML = "Please enter valid email!"
+        document.getElementById('mail-span').style.color =  "red";
+    }
+    if (massage < 9) {
+        document.getElementById("tel-span").innerHTML = "Please enter valid phone number!"
+        return document.getElementById("tel-span").style.color =  "red";
+    }
+
+        Email.send({
+        Host : "smtp.gmail.com",
+        Username : "e.goldman@hackeru.co.il",
+        Password : "",
+        To : 'e.goldman@hackeru.co.il',
+        From : "e.goldman@hackeru.co.il",
+        Subject : `Mail from: ${firstName} ${lastName}`,
+        Body : "Thank you for filling out the form"
+    }).then(
+      () => {
+          document.getElementById('btn-id').innerHTML = `The message has been sent:`;
+          document.getElementById('btn-id').style.color =  "green";
+        }
+    );
+
+    document.getElementById("name-id").value = '';
+    document.getElementById("last-id").value= '';
+    document.getElementById("mail-id").value = '';
+    document.getElementById("tel-id").value = '';
+    document.getElementById('subject-id').value = '';
+    document.getElementById('mas-id').value = '';
+}
+
