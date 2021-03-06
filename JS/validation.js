8f76f77b0339f854e87125165972e48e887e5f0e
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2021-03-06 13:42:05 +0200

    2021/03/06 13:42

--- JS/validation.js
+++ JS/validation.js
@@ -4,28 +4,40 @@ function onSubClick(event) {
     let firstName = document.getElementById("name-id").value;
     let lastName = document.getElementById("last-id").value;
     let email = document.getElementById("mail-id").value;
-    let massage = document.getElementById("tel-id").value;
+    let phone = document.getElementById("tel-id").value;
+    let subject = document.getElementById("subject-id").value;
+    let message = document.getElementById("mas-id").value;
 
     document.getElementById('name-span').innerHTML = '';
     document.getElementById('last-span').innerHTML = '';
     document.getElementById('mail-span').innerHTML = '';
     document.getElementById('tel-span').innerHTML = '';
+    document.getElementById('subject-span').innerHTML = '';
+    document.getElementById('mes-span').innerHTML = '';
 
     if (firstName < 2) {
         document.getElementById('name-span').innerHTML = "Your First Name Is Too Short!"
-       document.getElementById('name-span').style.color =  "red";
+        return document.getElementById('name-span').style.color =  "red";
     }
     if (lastName < 2) {
          document.getElementById('last-span').innerHTML = "Your Last Name Is Too Short!"
-        document.getElementById('last-span').style.color =  "red";
+         return document.getElementById('last-span').style.color =  "red";
     }
     if (email.indexOf("@") == -1 || email.indexOf(".") == -1  ) {
         document.getElementById('mail-span').innerHTML = "Please enter valid email!"
-        document.getElementById('mail-span').style.color =  "red";
+        return document.getElementById('mail-span').style.color =  "red";
     }
-    if (massage < 9) {
+    if (phone < 9) {
         document.getElementById("tel-span").innerHTML = "Please enter valid phone number!"
         return document.getElementById("tel-span").style.color =  "red";
+    }
+    if (subject < 2) {
+        document.getElementById('subject-span').innerHTML = "Please enter the subject of the message!"
+        return document.getElementById('subject-span').style.color =  "red";
+    }
+    if (message < 2) {
+        document.getElementById('mes-span').innerHTML = "Please write the message!"
+       return document.getElementById('mes-span').style.color =  "red";
     }
 
         Email.send({
